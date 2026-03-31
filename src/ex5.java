f348a190b1a67744da79ea2061dc5820469d7e8b
Laboratório FIAP <labsfiap@fiap.com.br>
2026-03-30 22:42:23 -0300

    Exercicio 5 finalizado

---
+++ src/ex5.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class ex5 {
+
+    public static void main(String[] args) {
+        double valorCompra, desconto;
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("digite o valor total da compra: ");
+        valorCompra = sc.nextDouble();
+
+        if (valorCompra > 1000) {
+            desconto = valorCompra * 0.85;
+            System.out.println("Desconto de 15%: " + desconto);
+        }
+
+        else{
+            desconto = valorCompra * 0.92;
+            System.out.println("Desconto de 8%: " + desconto);
+        }
+    }
+}
